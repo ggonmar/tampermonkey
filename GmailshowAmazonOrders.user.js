affc7410eb5dfc0aa72ddf8537281f9c3095ec28
ggonmar <ggonmar@users.noreply.github.com>
2020-12-03 16:00:39 +0100

    Update GmailshowAmazonOrders.user.js
    Now we store the the item on storage to avoid calling to amazon a bit too much

--- GmailshowAmazonOrders.user.js
+++ GmailshowAmazonOrders.user.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name         Gmail show Amazon orders
-// @version      0.1
+// @version      1.0
 // @description  Isn't it annoying to have to switch context between gmail and amazon every time? This will solve it for you. https://twitter.com/ggonmar/status/1334461580759740416
 // @author       Guillermo (ggonmar@gmail.com)
 // @include      https://mail.google.com/*
 // @grant        GM.xmlHttpRequest
-// @require http://code.jquery.com/jquery-3.4.1.min.js
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @grant        GM.deleteValue
+// @grant        GM.listValues
 // ==/UserScript==
 
 /*
@@ -16,7 +19,6 @@ let img = document.createElement("IMG");
 img.src=goodLogo;
 //document.getElementById('body').appendChild(img);
 
-console.log('HOLI');
 function findCurrentUnread(){
     var aTags = document.getElementsByTagName("div");
     var found;
@@ -34,7 +36,6 @@ function findCurrentUnread(){
     let refNumberFormat = /\d{3}-\d{7}-\d{7}/g;
 
     await untilGmailIsFullyLoaded();
-    console.log('yay, start doing things');
 
     attachPanel();
     setInterval(lookForAmazonReferences, 200);
@@ -55,13 +56,41 @@ function findCurrentUnread(){
     function addEventOn(element) {
 
         element.onmouseover = async function () {
-            this.style.cursor="help";
+            this.style.cursor = "help";
             await populateDivWithCorrectData(this);
         };
         element.onmouseout = function () {
             emptyDivAndHide(this);
         };
 
+    }
+
+    async function findCorrectValueForReference(ref) {
+        let html = (await GM.getValue(ref, "")).code;
+        if (!html) {
+            let webHTML = await GM.xmlHttpRequest({
+                method: 'GET',
+                url: `https://www.amazon.es/gp/your-account/order-details/ref=ppx_yo_dt_b_order_details_o00?ie=UTF8&orderID=${ref}#orderDetails`
+            });
+
+            if (webHTML.status == 200) {
+                let parser = new DOMParser();
+                webHTML = (parser.parseFromString(webHTML.responseText.trim(), "text/html"))
+                    .documentElement;
+
+                ['nav-belt', 'nav-main', 'nav-subnav', 'skiplink', 'recsWidget', 'navFooter']
+                    .forEach((e) => webHTML.querySelector(`#${e}`).remove());
+                [{nth: 0,  elem: 'breadcrumbs'}].forEach((e) => webHTML.querySelectorAll(`.${e.elem}`)[e.nth].remove());
+
+                webHTML.querySelector('#orderDetails').style.width="90%";
+                let now =new Date();
+                let expiryDate = new Date(now.setMonth(now.getMonth()+1));
+
+                await GM.setValue(ref, {code: webHTML.outerHTML, expiry: expiryDate});
+                html = webHTML.outerHTML;
+            }
+        }
+        return html;
     }
 
     async function populateDivWithCorrectData(information) {
@@ -77,26 +106,7 @@ function findCurrentUnread(){
             div.style.left = `${(parseFloat(div.style.left) - 5)}px`;
         }
 
-        return await GM.xmlHttpRequest({
-            method: 'GET',
-            url: `https://www.amazon.es/gp/your-account/order-details/ref=ppx_yo_dt_b_order_details_o00?ie=UTF8&orderID=${referenceNumber}#orderDetails`,
-            onload: function (response) {
-                if (response.status == 200) {
-                    div.innerHTML = response.responseText;
-                    ['nav-belt', 'nav-main', 'nav-subnav', 'skiplink', 'recsWidget', 'navFooter']
-                        .forEach((e) => document.getElementById(e).remove());
-                    [{
-                        nth: 0,
-                        elem: 'breadcrumbs'
-                    }].forEach((e) => document.getElementsByClassName(e.elem)[e.nth].remove());
-
-                    document.getElementById('orderDetails').style.width="90%";
-                }
-            },
-            onerror: function (response) {
-                div.innerHTML = `An error occurred while retrieving order ${referenceNumber}`;
-            }
-        });
+        div.innerHTML = await findCorrectValueForReference(referenceNumber);
     }
 
     let getElemDistanceToTop = function (elem) {
@@ -122,18 +132,18 @@ function findCurrentUnread(){
 
     function emptyDivAndHide(elem) {
         let currently = document.querySelectorAll(":hover");
-        elem.style.cursor="auto";
-        if (!Array.from(currently).some( e=> e.id =="amazon-info")) {
+        elem.style.cursor = "auto";
+        if (!Array.from(currently).some(e => e.id == "amazon-info")) {
             clearUp();
         }
     }
 
-    function clearUp()
-    {
+    function clearUp() {
         let div = document.querySelector('#amazon-info');
         div.style.display = "none";
         div.innerHTML = "";
     }
+
     async function untilGmailIsFullyLoaded() {
         while (!Array.from(document.querySelectorAll('a')).filter(e => {
             return e.innerText.match(/Inbox/g)
@@ -145,15 +155,26 @@ function findCurrentUnread(){
     function attachPanel() {
         let elem = document.createElement('div');
         elem.id = "amazon-info";
-        elem.style.cssText = 'display:none;position:absolute;top:100px;left:10%;width:60%;height:80%;opacity:0.85;border:5px outset grey; z-index:100;background:#fff';
+        elem.style.cssText = 'display:none;position:absolute;top:100px;left:10%;width:60%;height:80%;opacity:0.92;border:5px outset grey; z-index:100;background:#fff';
         elem.innerHTML = "Information goes here";
         elem.onmouseout = function () {
             clearUp();
         }
         document.body.appendChild(elem);
-
+        cleanUpExpiredEntriesInStorage().then();
     }
 
+    async function cleanUpExpiredEntriesInStorage(){
+        let storedValues= await GM.listValues();
+
+        let now = new Date();
+        for (const entry of storedValues) {
+            let entryValue = await GM.getValue(entry);
+            let expiryDate = new Date(entryValue.expiry);
+            if(now < expiryDate)
+                GM.deleteValue(entry);
+        }
+    }
 
     let spinner = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin:auto;background:#fff;display:block;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
 <g transform="rotate(0 50 50)">
