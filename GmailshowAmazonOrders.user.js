03f3d3dac4d40c8a113a150f4d2cdbce81488ff1
Guillermo Gonzalez Martin <gmartin@netsuite.com>
2021-02-24 15:59:28 +0100

    Forcing panel to never have scrollbar

--- GmailshowAmazonOrders.user.js
+++ GmailshowAmazonOrders.user.js
@@ -18,21 +18,21 @@
 
     let refNumberFormat = /\d{3}-\d{7}-\d{7}/g;
     let daysToStoreReferences = 7;
-    let debug=false;
-    let sticky=false;
+    let debug = false;
+    let sticky = false;
     let amazonInstance = await GM.getValue('AmazonInstance');
-    if(!amazonInstance) amazonInstance="es";
+    if (!amazonInstance) amazonInstance = "es";
 
     await untilGmailIsFullyLoaded();
 
     attachPanel();
     setInterval(lookForAmazonReferences, 200);
-    window.onhashchange = function() {
+    window.onhashchange = function () {
         clearUp(true);
     }
 
     function lookForAmazonReferences() {
-        if(window.location.href.endsWith("#inbox")) return;
+        if (window.location.href.endsWith("#inbox")) return;
 
         let amazonReferences = Array.from(document.querySelectorAll('a,h2,span')).filter(e => {
             return e.innerText.match(refNumberFormat)
@@ -51,22 +51,22 @@
             await populateDivWithCorrectData(this);
         };
         element.onmouseout = function () {
-            if(!sticky)
+            if (!sticky)
                 emptyDivAndHide(this);
         };
-        element.onclick = function(){
+        element.onclick = function () {
             makeSticky(this);
         }
     }
 
-    function makeSticky(element){
+    function makeSticky(element) {
         let div = document.querySelector('#amazon-info');
-        if(div.style.display =="" && sticky ==true){
-            sticky=false;
+        if (div.style.display == "" && sticky == true) {
+            sticky = false;
             clearUp(true);
             return;
         }
-        if(div.style.display == "")
+        if (div.style.display == "")
             sticky = true;
     }
 
@@ -83,28 +83,27 @@
                 let parser = new DOMParser();
                 webHTML = (parser.parseFromString(webHTML.responseText.trim(), "text/html"))
                     .documentElement;
-                try{
+                try {
                     ['nav-belt', 'nav-main', 'nav-subnav', 'skiplink', 'recsWidget', 'navFooter', 'orderDetails h1']
                         .forEach((e) => {
-                            if(webHTML.querySelector(`#${e}`))
+                            if (webHTML.querySelector(`#${e}`))
                                 webHTML.querySelector(`#${e}`).remove()
                         });
                     [{elem: 'breadcrumbs', nth: 0,}, {elem: 'hide-if-js', nth: 0,}, {elem: 'a-last', nth: 0,}]
-                            .forEach((e) => {
-                                if(webHTML.querySelectorAll(`.${e.elem}`).length > e.nth)
-                                    webHTML.querySelectorAll(`.${e.elem}`)[e.nth].remove()
-                            });
+                        .forEach((e) => {
+                            if (webHTML.querySelectorAll(`.${e.elem}`).length > e.nth)
+                                webHTML.querySelectorAll(`.${e.elem}`)[e.nth].remove()
+                        });
 
-                    if(webHTML.querySelector(`#a-popover-invoiceLinks`))
+                    if (webHTML.querySelector(`#a-popover-invoiceLinks`))
                         webHTML.querySelector(`#a-popover-invoiceLinks`).parentNode.remove()
 
-                    webHTML.querySelector('#orderDetails').style.width="90%";
+                    webHTML.querySelector('#orderDetails').style.width = "90%";
                     html = webHTML.outerHTML;
-                    let now =new Date();
-                    let expiryDate = new Date(now.setDate(now.getDate()+daysToStoreReferences));
+                    let now = new Date();
+                    let expiryDate = new Date(now.setDate(now.getDate() + daysToStoreReferences));
                     await GM.setValue(ref, {code: html, expiry: expiryDate});
-                }
-                catch(e){
+                } catch (e) {
                     html = webHTML.outerHTML;
                 }
 
@@ -113,9 +112,8 @@
 
         let parser = new DOMParser();
         let webHTML = (parser.parseFromString(html, "text/html")).documentElement;
-        for(let elem of webHTML.querySelectorAll('[href]'))
-        {
-            if(elem.getAttribute('href')[0] === "/")
+        for (let elem of webHTML.querySelectorAll('[href]')) {
+            if (elem.getAttribute('href')[0] === "/")
                 elem.setAttribute('href', `https://www.amazon.${amazonInstance}${elem.getAttribute('href')}`);
         }
 
@@ -126,17 +124,17 @@
         let referenceNumber = information.innerText.match(refNumberFormat)[0];
         let div = document.querySelector('#amazon-info');
         let existingContent = document.querySelector('#amazon-info-content');
-        if(existingContent) existingContent.remove();
+        if (existingContent) existingContent.remove();
 
-        let content=document.createElement('div');
-        content.id='amazon-info-content';
+        let content = document.createElement('div');
+        content.id = 'amazon-info-content';
         content.style.cssText = "margin-top:-1.5em;";
         content.innerHTML = `<div>${spinner}</div><div style="width:100%;text-align:center">Loading information for ${referenceNumber}</div>`;
 //        console.log(information);
         div.appendChild(content);
 
         div.style.display = "";
-        div.style.top = getElemDistanceToTop(information) < window.innerHeight/2 ?
+        div.style.top = getElemDistanceToTop(information) < window.innerHeight / 2 ?
             `${getElemDistanceToTop(information) + information.offsetHeight + 5}px`
             :
             `${getElemDistanceToTop(information) - div.offsetHeight - information.offsetHeight}px`
@@ -144,13 +142,20 @@
         while (window.innerWidth - div.getBoundingClientRect().right < 100) {
             div.style.left = `${(parseFloat(div.style.left) - 5)}px`;
         }
-
         content.innerHTML = await composeContentForReference(referenceNumber);
 
         while (window.innerHeight - div.getBoundingClientRect().bottom < 100) {
             div.style.top = `${(parseFloat(div.style.top) - 5)}px`;
         }
 
+        while (div.scrollHeight > div.clientHeight) {
+            if (div.style.zoom === "")
+                div.style.zoom = "0.99";
+            else {
+                let newZoom = parseFloat(div.style.zoom) - 0.01;
+                div.style.zoom = `${newZoom}`;
+            }
+        }
     }
 
     let getElemDistanceToTop = function (elem) {
@@ -182,12 +187,11 @@
         }
     }
 
-    function clearUp(force=false) {
+    function clearUp(force = false) {
         let div = document.querySelector('#amazon-info');
-        let content=document.querySelector('#amazon-info-content');
-        if(div.style.display =="none") return;
-        if((debug || sticky) && !force) return;
-        if(div.offsetHeight+10 < content.offsetHeight && !force) return;
+        let content = document.querySelector('#amazon-info-content');
+        if (div.style.display == "none") return;
+        if ((debug || sticky) && !force) return;
         div.style.display = "none";
         content.remove();
     }
@@ -203,13 +207,13 @@
     function attachPanel() {
         let elem = document.createElement('div');
         elem.id = "amazon-info";
-        elem.style.cssText = 'display:none;position:absolute;top:100px;left:10%;width:60%;max-height:65%; overflow-y: auto;opacity:0.92;border:5px outset grey; z-index:100;background:#fff';
+        elem.style.cssText = 'display:none;position:absolute;top:100px;left:10%;width:60%;max-height:65%; overflow-y: auto;opacity:0.92;border:5px outset grey; z-index:100;background:#fff;zoom:1';
         elem.innerHTML = "";
-        let close=document.createElement('div');
-        close.id="amazom-info-close";
-        close.style.cssText='position:absolute;top:10px;right:20px;cursor:pointer';
-        close.innerText='x';
-        close.onclick=function(){
+        let close = document.createElement('div');
+        close.id = "amazom-info-close";
+        close.style.cssText = 'position:absolute;top:10px;right:20px;cursor:pointer';
+        close.innerText = 'x';
+        close.onclick = function () {
             sticky = false;
             clearUp(true);
         }
@@ -223,49 +227,51 @@
 
     }
 
-    async function cleanUpExpiredEntriesInStorage(){
-        let storedValues= (await GM.listValues()).filter(e => e != "AmazonInstance");
+    async function cleanUpExpiredEntriesInStorage() {
+        let storedValues = (await GM.listValues()).filter(e => e != "AmazonInstance");
         let now = new Date();
         for (const entry of storedValues) {
             let entryValue = await GM.getValue(entry);
             let expiryDate = new Date(entryValue.expiry);
-            if(now > expiryDate)
+            if (now > expiryDate)
                 GM.deleteValue(entry);
         }
     }
 
-    async function cleanUpStorage(){
-      let storedValues= (await GM.listValues()).filter(e => e != "AmazonInstance");
-      for (const entry of storedValues) {
-          GM.deleteValue(entry);
-      }
+    async function cleanUpStorage() {
+        let storedValues = (await GM.listValues()).filter(e => e != "AmazonInstance");
+        for (const entry of storedValues) {
+            GM.deleteValue(entry);
+        }
     }
 
-    async function changeAmazonInstance(){
+    async function changeAmazonInstance() {
         let option;
-        let continueOnLoop=false;
-        do{
-            let str="What amazon domain would you like to launch queries against?";
-            if(option != undefined) str += `\nThe domain "amazon.${option}" is invalid.`;
-            option=window.prompt(str);
+        let continueOnLoop = false;
+        do {
+            let str = "What amazon domain would you like to launch queries against?";
+            if (option != undefined) str += `\nThe domain "amazon.${option}" is invalid.`;
+            option = window.prompt(str);
             console.log(option);
-            continueOnLoop = !(option==null || option.length == 0 || verifyValidAmazonDomain(option));
-        }while(continueOnLoop)
-        if(option){
+            continueOnLoop = !(option == null || option.length == 0 || verifyValidAmazonDomain(option));
+        } while (continueOnLoop)
+        if (option) {
             await GM.setValue('AmazonInstance', option)
             location.reload()
         }
     }
 
-    function verifyValidAmazonDomain(domain){
-        if(domain == "es" || domain == "it")
+    function verifyValidAmazonDomain(domain) {
+        if (domain == "es" || domain == "it")
             return true;
         else
             return false;
         let webHTML = GM.xmlHttpRequest({
             method: 'GET',
             url: `https://www.amazon.${amazonInstance}/gp/your-account/order-details/ref=ppx_yo_dt_b_order_details_o00?ie=UTF8&orderID=${ref}#orderDetails`
-        }).then(e => {return e});
+        }).then(e => {
+            return e
+        });
     }
 
     let spinner = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin:auto;margin-top:2em;background:#fff;display:block;" width="100px" height="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
